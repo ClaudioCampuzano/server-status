6dda2c064f6f2c5ac19aeffbcf9f7285b53e2d9e
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-10-22 10:36:28 -0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,2 +1,2 @@
 source env/bin/activate
-python3 status.py -n QN  -f /home/omia/Documents/logs/faust.log -d /home/omia/Documents/logs/flujo_quicentro_norte.log
+python3 status.py -n QN -f /home/omia/Documents/logs/faust.log -d /home/omia/Documents/logs/flujo_quicentro_norte.log
