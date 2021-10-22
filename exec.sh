e23daa707376bcefd745e6c4e618ca7ca999953f
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-10-22 10:59:19 -0300

    Update exec.sh

--- exec.sh
+++ exec.sh
@@ -1,2 +1,3 @@
-source env/bin/activate
-python3 status.py -n QN -f /home/omia/Documents/logs/faust.log -d /home/omia/Documents/logs/flujo_quicentro_norte.log
+myloc=$(realpath "$0" | sed 's|\(.*\)/.*|\1|')
+source $myloc/env/bin/activate
+python3 $myloc/status.py -n QN -f /home/omia/Documents/logs/faust.log -d /home/omia/Documents/logs/flujo_quicentro_norte.log
