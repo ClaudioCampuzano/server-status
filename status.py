8de58a1785ca40729b8288a9563d71500bc8c68f
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-11-03 17:04:23 -0300

    Update status.py

--- status.py
+++ status.py
@@ -11,11 +11,44 @@ def sendSlackMsg(message):
     else:
         print('Error')
 
+def getInfoDs(filename, analyticalType):
+    Ds_process=analyticalType+' Ds:\n'
+    gatillo = False
+    try:
+        for line in reversed(list(open(filename))):
+            if gatillo:
+                Ds_process += '\n'+line.rstrip()
+                break
+            if line.split(" ")[0].rstrip() == '**PERF:':
+                Ds_process += line.rstrip()
+                gatillo = True
+        stringList = Ds_process.split('\n')
+        return stringList[0]+'\n'+stringList[2]+'\n'+stringList[1]+'\n'
+    except:
+        print('Problemas al abrir el archivo '+analyticalType+' Ds Log')
+        return Ds_process
+
+def getInfoFaust(filename, analyticalType):
+    Faust_process = analyticalType+' Faust:\n'
+    try:
+        for index, line in enumerate(reversed(list(open(filename)))):
+            if index < 3:
+                Faust_process += line.rstrip() + '\n'
+            else:
+                break
+    except:
+        print('Problemas al abrir el archivo '+analyticalType+' Faust Log')
+        Faust_process += ''
+    return Faust_process
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Program that reports metrics from the computer to Slack')
     parser.add_argument('-n', '--mallName', type=str, required=True, help='name of the mall to report')
-    parser.add_argument('-f', '--logFaust', type=str, required=True, help='Absolute address of the Faust log')
-    parser.add_argument('-d', '--logDs', type=str, required=True, help='Absolute address of the Faust log')
+    parser.add_argument('-a', '--logFlujoFaust', type=str, required=True, help='Absolute address of the flujo Faust log')
+    parser.add_argument('-b', '--logFlujoDs', type=str, required=True, help='Absolute address of the flujo Ds log')
+    parser.add_argument('-c', '--logAforoFaust', type=str, required=True, help='Absolute address of the Aforo Faust log')
+    parser.add_argument('-d', '--logAforoDs', type=str, required=True, help='Absolute address of the Aforo Ds log')
+
     args = parser.parse_args()
 
     gpu = gputil.getGPUs()
@@ -33,14 +66,9 @@ if __name__ == '__main__':
     diskFree = f"disk free: {round(disk.free/(1024**3))}GiB"
     mallName = args.mallName+':'
 
-    for line in reversed(list(open(args.logDs))):
-        if line.split(" ")[0].rstrip() == '**PERF:':
-            Ds_process = 'Ds: '+line.rstrip()
-            break
-    Faust_process = 'Faust: '
-    for index, line in enumerate(reversed(list(open(args.logFaust)))):
-        if index < 5:
-            Faust_process += line.rstrip() + '\n'
-        else:
-            break
-    sendSlackMsg("\n".join([mallName,gpu,gpuMem,temp,ramUsed,diskFree,Ds_process, Faust_process]))
+    flujoFaust = '\n'+getInfoFaust(args.logFlujoFaust,'#--Flujo--#')
+    aforoFaust = getInfoFaust(args.logAforoFaust,'#--Aforo--#')
+    flujoDs = getInfoDs(args.logFlujoDs,'#--Flujo--#')
+    aforoDs = getInfoDs(args.logAforoDs,'#--Aforo--#')
+
+    sendSlackMsg("\n".join([mallName,gpu,gpuMem,temp,ramUsed,diskFree,flujoFaust,flujoDs,aforoFaust,aforoDs]))
