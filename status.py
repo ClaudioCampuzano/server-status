226e800ab1bb93eb26176738d0d0d501a1c5013e
Claudio Campuzano <claudio.campuzano.b@gmail.com>
2021-11-04 11:10:10 -0300

    Update status.py

--- status.py
+++ status.py
@@ -12,7 +12,7 @@ def sendSlackMsg(message):
         print('Error')
 
 def getInfoDs(filename, analyticalType):
-    Ds_process=analyticalType+' Ds:\n'
+    Ds_process='*'+analyticalType+'* Ds:\n'
     gatillo = False
     try:
         for line in reversed(list(open(filename))):
@@ -23,13 +23,13 @@ def getInfoDs(filename, analyticalType):
                 Ds_process += line.rstrip()
                 gatillo = True
         stringList = Ds_process.split('\n')
-        return stringList[0]+'\n'+stringList[2]+'\n'+stringList[1]+'\n'
+        return stringList[0]+'\n ```'+stringList[2]+'\n'+stringList[1]+'``` \n'
     except:
         print('Problemas al abrir el archivo '+analyticalType+' Ds Log')
         return Ds_process
 
 def getInfoFaust(filename, analyticalType):
-    Faust_process = analyticalType+' Faust:\n'
+    Faust_process = '*'+analyticalType+'* Faust:\n ```'
     try:
         for index, line in enumerate(reversed(list(open(filename)))):
             if index < 3:
@@ -39,7 +39,7 @@ def getInfoFaust(filename, analyticalType):
     except:
         print('Problemas al abrir el archivo '+analyticalType+' Faust Log')
         Faust_process += ''
-    return Faust_process
+    return Faust_process+'```'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Program that reports metrics from the computer to Slack')
@@ -64,11 +64,13 @@ if __name__ == '__main__':
     temp = f"temp: {gpu_temp}C"
     ramUsed = f"ram used: {round(ram.used/(1024**3))}GiB"
     diskFree = f"disk free: {round(disk.free/(1024**3))}GiB"
-    mallName = args.mallName+':'
+    systemUtils=['```',gpu,gpuMem,temp,ramUsed,diskFree,'```\n']
 
-    flujoFaust = '\n'+getInfoFaust(args.logFlujoFaust,'#--Flujo--#')
+    mallName = '>*'+args.mallName+'*:'
+
+    flujoFaust = getInfoFaust(args.logFlujoFaust,'#--Flujo--#')
     aforoFaust = getInfoFaust(args.logAforoFaust,'#--Aforo--#')
     flujoDs = getInfoDs(args.logFlujoDs,'#--Flujo--#')
     aforoDs = getInfoDs(args.logAforoDs,'#--Aforo--#')
 
-    sendSlackMsg("\n".join([mallName,gpu,gpuMem,temp,ramUsed,diskFree,flujoFaust,flujoDs,aforoFaust,aforoDs]))
+    sendSlackMsg("\n".join([mallName]+systemUtils+[flujoFaust,flujoDs,aforoFaust,aforoDs]+['*--------------------------------------------------------------------*']))
